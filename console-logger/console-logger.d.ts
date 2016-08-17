2de4a6217ba1dce6ade0cc9d291eefbf5d3c009b
Duarte Mechas <dmechas@equalexperts.com>
2016-08-17 11:41:41 +0100

    remove unused typings

--- console-logger/console-logger.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "console-logger" {
-  import * as winston from "winston";
-
-  function createLogger(logLevel: string, component: string, env: string, module: string): winston.LoggerInstance;
-
-  export = createLogger;
-}
--- http-proxy-middleware/http-proxy-middleware.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "http-proxy-middleware" {
-  import express = require("express");
-  function proxy(context: Object, opts?: Object):
-    (req: express.Request, res: express.Response, next: express.NextFunction) => any;
-  export = proxy;
-}
