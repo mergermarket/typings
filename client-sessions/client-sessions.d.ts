a3ecd27ba9e8076481d81fb85b03857bc40c7fb1
Duarte Mechas <dmechas@equalexperts.com>
2016-08-18 14:05:12 +0100

    add client-sessions typing definition

---
+++ client-sessions/client-sessions.d.ts
@@ -0,0 +1,28 @@
+declare module "client-sessions" {
+  import * as express from "express";
+
+  namespace clientsessions {
+    interface ICookie {
+      ephemeral?: boolean;
+      httpOnly?: boolean;
+      maxAge?: number;
+      secure?: boolean;
+      [key: string]: any;
+    }
+
+    interface IOptions {
+      secret?: string;
+      encryptionKey?: NodeJS.Buffer;
+      signatureKey?: NodeJS.Buffer;
+      cookieName?: string;
+      duration?: number;
+      activeDuration?: number;
+      encryptionAlgorithm?: string;
+      signatureAlgorithm?: string;
+      cookie?: clientsessions.ICookie;
+      requestKey?: string;
+    }
+  }
+
+  function clientSessionFactory(opts: clientsessions.IOptions): express.RequestHandler;
+}
